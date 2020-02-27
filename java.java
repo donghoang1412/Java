393a14be5db7282636b9cf8de0a1a78e232906bd
donghoang1412 <donghoang1412@gmail.com>
2020-02-27 13:56:56 -0600

    add return Sum method

--- java.java
+++ java.java
@@ -2,5 +2,10 @@ public class java {
     public static void main(String[] args)
     {
         System.out.println("Hello World");
+        System.out.println(returnSum(10,20));
+    }
+    public int returnSum (int a, int b)
+    {
+        return a+b;
     }
 }
