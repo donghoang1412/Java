e33a90fb8f8d89c58d307b3b1e3047af89613419
Tien Nguyen <tien.ng1020@gmail.com>
2020-02-27 14:25:04 -0600

    added return difference

--- java.java
+++ java.java
@@ -3,9 +3,14 @@ public class java {
     {
         System.out.println("Hello World");
         System.out.println(returnSum(10,20));
+        System.out.println(returnDifferent(40,36));
     }
     public int returnSum (int a, int b)
     {
         return a+b;
+    }
+    public int returnDifferent (int x, int y)
+    {
+        return x-y;
     }
 }
